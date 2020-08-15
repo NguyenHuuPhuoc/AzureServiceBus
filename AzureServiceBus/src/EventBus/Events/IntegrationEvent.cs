e4503fff820d6de95a54d9dd87d713c1b66368b9
Phước <>
2020-08-15 20:13:35 +0700

    Implement azure service bus

---
+++ AzureServiceBus/src/EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace EventBus.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        [JsonProperty]
+        public Guid Id { get; set; }
+
+        [JsonProperty]
+        public DateTime CreationDate { get; set; }
+    }
+}
